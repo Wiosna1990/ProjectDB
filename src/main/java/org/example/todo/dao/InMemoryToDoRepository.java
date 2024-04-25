0923435b49f4e238b9d8d95fb82346d74c843051
lujna <zanzarah@op.pl>
2024-04-25 20:06:33 +0200

    Improve Methods and Tests in InMemoryToDoRepository

--- src/main/java/org/example/todo/dao/InMemoryToDoRepository.java
+++ src/main/java/org/example/todo/dao/InMemoryToDoRepository.java
@@ -1,25 +1,10 @@
 package org.example.todo.dao;
-
 import org.example.todo.service.ToDo;
-
-import java.time.Clock;
-import java.time.LocalDateTime;
 import java.util.*;
 
 public class InMemoryToDoRepository implements ToDoRepository {
 
-    private final String action;
-    private final Clock clock;
-    private final UUID uuid;
-
-    public InMemoryToDoRepository(String action, Clock clock, UUID uuid) {
-        this.action = action;
-        this.clock = clock;
-        this.uuid = uuid;
-    }
-
-
-    Map<UUID, ToDo> toDoMap = new LinkedHashMap<>();
+    private final Map<UUID, ToDo> toDoMap = new LinkedHashMap<>();
 
     public void save(ToDo todo) {
 
@@ -32,15 +17,13 @@ public class InMemoryToDoRepository implements ToDoRepository {
     }
 
     public void remove(UUID uuid) {
+        Objects.requireNonNull(uuid);
 
-        if (toDoMap.containsKey(uuid)) {
-            toDoMap.remove(uuid);
-        } else if (uuid == null) {
-
-            throw new NullPointerException();
-        } else if (!toDoMap.containsKey(uuid)) {
-
+        if (toDoMap.remove(uuid) == null) {
             throw new NoSuchElementException("Brak elementu o UUID <" + uuid + ">");
         }
     }
+
+
 }
+
--- src/test/java/org/example/todo/dao/InMemoryToDoRepositoryTest.java
+++ src/test/java/org/example/todo/dao/InMemoryToDoRepositoryTest.java
@@ -3,13 +3,8 @@ package org.example.todo.dao;
 import org.example.todo.service.ToDo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.time.Clock;
-import java.time.Instant;
 import java.time.LocalDateTime;
-import java.time.ZoneId;
 import java.util.*;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
 
@@ -21,27 +16,28 @@ class InMemoryToDoRepositoryTest {
             "zadanie1",
             LocalDateTime.of(2023, 3, 18, 17, 17, 30)
     );
-    private static final Map<UUID, ToDo> TEST_MAP = new LinkedHashMap<UUID, ToDo>() {{
-        put(UUID.fromString("72331291-2088-4FA7-BE89-D2A680FF1B69"),
-                new ToDo(UUID.fromString("72331291-2088-4FA7-BE89-D2A680FF1B69"),
-                        "zadanie2",
-                        LocalDateTime.of(2024, 3, 18, 17, 17, 30)));
+    private static final ToDo TEST_TODO2 = new ToDo(
+            UUID.fromString("72331291-2088-4FA7-BE89-D2A680FF1B69"),
+            "zadanie2",
+            LocalDateTime.of(2024, 3, 18, 17, 17, 30)
+    );
 
-    }};
+    private static final ToDo TEST_TODO3 = new ToDo(
+            UUID.fromString("72331291-2088-4FA7-BE89-D2A680FF1B10"),
+            "zadanie3",
+            LocalDateTime.of(2024, 3, 18, 17, 17, 30)
+    );
 
-    private Clock clock = Clock.fixed(Instant.parse("2023-03-18T17:17:30.00Z"), ZoneId.of("UTC"));
-    private UUID uuid = UUID.fromString("aacc4621-d88f-4a94-ae2f-b38072bf5087");
-    private String action = "zadanie1";
     private InMemoryToDoRepository inMemoryToDoRepository;
 
     //Given
     @BeforeEach
     void setUp() {
-        inMemoryToDoRepository = new InMemoryToDoRepository(action, clock, uuid);
+        inMemoryToDoRepository = new InMemoryToDoRepository();
     }
 
     @Test
-    void givenNullInSaveMethodShouldReturnNullPointerException() {
+    void givenNullInSaveMethodShouldThrowNullPointerException() {
         //when
         Throwable thrown = catchThrowable(() -> inMemoryToDoRepository.save(null));
 
@@ -54,15 +50,35 @@ class InMemoryToDoRepositoryTest {
         //when
         inMemoryToDoRepository.save(TEST_TODO);
 
-        TEST_MAP.put(TEST_TODO.uuid(), TEST_TODO);
+        inMemoryToDoRepository.save(TEST_TODO2);
 
         //then
-        assertThat(TEST_MAP).contains(Map.entry(TEST_TODO.uuid(), TEST_TODO));
+        assertThat(inMemoryToDoRepository.getAll()).contains(TEST_TODO2);
+    }
+
+    @Test
+    void givenToDoShouldReturnNotEmptyList(){
+        //when
+        inMemoryToDoRepository.save(TEST_TODO);
+
+        //then
+        assertThat(inMemoryToDoRepository.getAll()).isNotEmpty();
+    }
+
+    @Test
+    void givenNextToDoShouldReturnListWithMoreElements(){
+        //when
+        inMemoryToDoRepository.save(TEST_TODO);
+        int sizeOfList = inMemoryToDoRepository.getAll().size();
+        inMemoryToDoRepository.save(TEST_TODO3);
+
+        //then
+        assertThat(inMemoryToDoRepository.getAll().size()).isGreaterThan(sizeOfList);
     }
 
 
     @Test
-    void givenNullInRemoveMethodShouldReturnNullPointerException() {
+    void givenNullInRemoveMethodShouldThrowNullPointerException() {
         //when
         Throwable thrown = catchThrowable(() -> inMemoryToDoRepository.remove(null));
 
@@ -72,7 +88,7 @@ class InMemoryToDoRepositoryTest {
 
 
     @Test
-    void givenUnknownUuidInRemoveMethodShouldReturnNoSuchElementException() {
+    void givenUnknownUuidInRemoveMethodShouldThrowNoSuchElementException() {
         //when
         Throwable thrown = catchThrowable(() -> inMemoryToDoRepository.remove(TEST_UUID));
 
@@ -81,12 +97,36 @@ class InMemoryToDoRepositoryTest {
     }
 
     @Test
+    void givenUuidInRemoveMethodOnListWithOneElementShouldReturnEmptyList(){
+        //when
+        inMemoryToDoRepository.save(TEST_TODO);
+        inMemoryToDoRepository.remove(TEST_TODO.uuid());
+
+        //then
+        assertThat(inMemoryToDoRepository.getAll()).isEmpty();
+    }
+
+    @Test
+    void givenUuidInRemoveMethodOnListWithMoreElementsShouldReturnNotEmptyLists(){
+        //when
+        inMemoryToDoRepository.save(TEST_TODO);
+        inMemoryToDoRepository.save(TEST_TODO3);
+        int sizeOfList = inMemoryToDoRepository.getAll().size();
+        inMemoryToDoRepository.remove(TEST_TODO.uuid());
+
+        //then
+        assertThat(inMemoryToDoRepository.getAll().size()).isLessThan(sizeOfList);
+    }
+
+    @Test
     void givenListIsNotNull() {
         //when
         List<ToDo> result = inMemoryToDoRepository.getAll();
 
         //then
-        assertThat(result).isNotNull();
+        assertThat(result)
+                .isNotNull()
+                .isEmpty();
     }
 
 
